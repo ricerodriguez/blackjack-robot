3a06dd642de4320983a6cfdf1a7c38c447846a57
ricerodriguez <rice.rodriguez@ttu.edu>
2019-04-23 14:28:49 -0500

    tmptst now works with new camera

--- src/card_reader/test_alg.py
+++ src/card_reader/test_alg.py
@@ -3,8 +3,9 @@ import sys
 import cv2 as cv
 import numpy as np
 import logging as log
-from picamera import PiCamera
-cam = PiCamera()
+# from picamera import PiCamera
+# cam = PiCamera()
+cam = cv.VideoCapture(0)
 
 class RankSuitNotFound(Exception):
     ''' Raised when detecting that the suit or rank was not found '''
@@ -12,39 +13,29 @@ class RankSuitNotFound(Exception):
         self.expression = expression
         self.message = message
 
-def prep(im, mode=True):
+def prep(im,test, mode=True):
       # Convert to gray and blur it
     gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(gray,(0,0),2)
     # Adaptive thresholding
     thresh = cv.adaptiveThreshold(blur,255,1,1,11,1)
+    if test:
+        cv.imshow('Adaptive Thresholding',thresh)
+        cv.waitKey(0)
     kernel = np.ones((5,5),np.uint8)
     ero = cv.erode(thresh,kernel,iterations=1)
     if mode:
         # Gaussian blur
         dil = cv.dilate(ero,kernel,iterations=2)
+        if test:
+            cv.imshow('Dilation (2 iterations)',dil)
+            cv.waitKey(0)
     else:
         dil = cv.dilate(ero,kernel,iterations=3)
-    # if mode:
-    #     # Convert to gray and blur it
-    #     gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-    #     # Gaussian blur
-    #     blur = cv.GaussianBlur(gray,(5,5),2)
-    #     # Adaptive thresholding
-    #     thresh = cv.adaptiveThreshold(blur,255,1,1,11,1)
-    #     kernel = np.ones((5,5),np.uint8)
-    #     ero = cv.erode(thresh,kernel,iterations=1)
-    #     dil = cv.dilate(ero,kernel,iterations=1)
-    # else:
-    #     gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-    #     # Gaussian blur
-    #     blur = cv.GaussianBlur(gray,(9,9),2)
-    #     # Adaptive thresholding
-    #     thresh = cv.adaptiveThreshold(blur,255,1,1,11,1)
-    #     kernel = np.ones((5,5),np.uint8)
-    #     ero = cv.erode(thresh,kernel,iterations=1)
-    #     dil = cv.dilate(ero,kernel,iterations=1)
-        
+        if test:
+            cv.imshow('Dilation (3 iterations)',dil)
+            cv.waitKey(0)
+            
 
     return gray, blur, thresh, ero, dil
 
@@ -88,34 +79,40 @@ def find_rot_box(contours):
     return boxes, rects
 
         
-def canny_sort(mode, im):
-    _, _, _, _, dil = prep(im,mode)
+def canny_sort(mode, test, im):
+    _, _, _, _, dil = prep(im,test,mode)
     edges = cv.Canny(dil, 0, 255)
     mask = edges != 0
     dst = im * (mask[:,:,None].astype(im.dtype))
     _, contours, _ = cv.findContours(edges,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
     cont_sort = sorted(contours,key=cv.contourArea,reverse=True)[:1]
-    if ((cont_sort[-1].size > 350) or cont_sort[-1].size < 200) and mode:
-        raise RankSuitNotFound('rank', str(cont_sort[-1].size))
-    elif ((cont_sort[-1].size > 400) or cont_sort[-1].size < 200) and not mode:
-        raise RankSuitNotFound('suit', str(cont_sort[-1].size))
+    if not test:
+        if ((cont_sort[-1].size > 350) or cont_sort[-1].size < 200) and mode:
+            raise RankSuitNotFound('rank', str(cont_sort[-1].size))
+        elif ((cont_sort[-1].size > 400) or cont_sort[-1].size < 200) and not mode:
+            raise RankSuitNotFound('suit', str(cont_sort[-1].size))
+        else:
+            return im, contours, edges, cont_sort
     else:
         return im, contours, edges, cont_sort
+        
 
-def find_rank_suit(mode,im=None):
+def find_rank_suit(mode,test,im=None):
     found = False
     if im is None:
-        cam.capture('test.jpg')
-        im = cv.imread('test.jpg')
+        # cam.capture('test.jpg')
+        # im = cv.imread('test.jpg')
+        s, im = cam.read()
         
     while not found:
         try:
-            im, conts, edges, conts_sort = canny_sort(mode,im)
+            im, conts, edges, conts_sort = canny_sort(mode,test,im)
             found = True
         except RankSuitNotFound as err:
             log.warning('The {0} found was of size {1}, which is too large to be correct. Trying again.'.format(err.expression, err.message))
-            cam.capture('test.jpg')
-            im = cv.imread('test.jpg')
+            # cam.capture('test.jpg')
+            # im = cv.imread('test.jpg')
+            s, im = cam.read()
             found = False
 
     return im, conts, edges, conts_sort
@@ -134,21 +131,24 @@ def crop_to_area(im,rect):
     warp = cv.warpPerspective(im,mat,(width,height))
     return warp    
 
-def find_card():
-    im, rank_conts, rank_edges, rank = find_rank_suit(True)
-    _, suit_conts, suit_edges, suit = find_rank_suit(False,im)
+def find_card(test, path=None):
+    im, rank_conts, rank_edges, rank = find_rank_suit(True,test)
+    _, suit_conts, suit_edges, suit = find_rank_suit(False,test,im)
     i = 0
 
     match = cv.matchShapes(rank[-1],suit[-1],cv.CONTOURS_MATCH_I1,420.69)
-    while (match < 1 or match > 2):
-        match = cv.matchShapes(rank[-1],suit[-1],cv.CONTOURS_MATCH_I1,420.69)
-        log.warning('Match was {}. Accidentally found the same thing twice. Trying again.'.format(match))
-        i+=1
-        if (i >= 5):
-            im, rank_conts, rank_edges, rank = find_rank_suit(True)
-            _, suit_conts, suit_edges, suit = find_rank_suit(False,im)
-        else:
-            im, rank_conts, rank_edges, rank = find_rank_suit(True)
+    # if (len(sys.argv[1]) >= 2):
+    #     if not ('-t' in sys.argv[1]):
+    if not test:
+        while (match < 1 or match > 2):
+            match = cv.matchShapes(rank[-1],suit[-1],cv.CONTOURS_MATCH_I1,420.69)
+            log.warning('Match was {}. Accidentally found the same thing twice. Trying again.'.format(match))
+            i+=1
+            if (i >= 5):
+                im, rank_conts, rank_edges, rank = find_rank_suit(True,test)
+                _, suit_conts, suit_edges, suit = find_rank_suit(False,test,im)
+            else:
+                im, rank_conts, rank_edges, rank = find_rank_suit(True,test)
         
     print('rank: \n\t size: ',rank[-1].size,'\n\t shape: ',rank[-1].shape)
     print('suit: \n\t size: ',suit[-1].size,'\n\t shape: ',suit[-1].shape)
@@ -177,22 +177,18 @@ def find_card():
     cv.imshow('test',suit_warp)
     cv.waitKey(0)
 
-    # print(rank_warp.shape)
-    # print(suit_warp.shape)
     max_x = max(rank_warp.shape[0], suit_warp.shape[0])
     max_y = max(rank_warp.shape[1], suit_warp.shape[1])
     max_z = max(rank_warp.shape[2], suit_warp.shape[2])
-    # print(max_x)
-    # print(max_y)
 
     sw_copy = suit_warp.copy()
     sw_copy.resize((max_x, max_y, max_z))
-    cv.imshow('suit copy',sw_copy)
-    cv.waitKey(0)
+    # cv.imshow('suit copy',sw_copy)
+    # cv.waitKey(0)
     rw_copy = rank_warp.copy()
     rw_copy.resize((max_x, rw_copy.shape[1], max_z))
-    cv.imshow('rank copy',rw_copy)
-    cv.waitKey(0)
+    # cv.imshow('rank copy',rw_copy)
+    # cv.waitKey(0)
 
     final_im = np.hstack((rw_copy, sw_copy))
     cv.imshow('test',final_im)
@@ -200,10 +196,24 @@ def find_card():
 
     print('final image: \n\t size: ',final_im.size,'\n\t shape: ',final_im.shape)
 
-    if not os.path.exists('images'):
-        os.makedirs('images')
+    if not path is None:
+        if not os.path.exists('images'):
+            os.makedirs('images')
+        cv.imwrite('images/{}'.format(path),final_im)
 
-    cv.imwrite('images/{}'.format(sys.argv[1]),final_im)
+    # if (len(sys.argv) == 2):
+    #     if not ('-t' in sys.argv[1]):
+    #         if not os.path.exists('images'):
+    #             os.makedirs('images')
+    #         cv.imwrite('images/{}'.format(sys.argv[1]),final_im)
+    #     else:
+    #         pass
     
 if __name__=='__main__':
-    find_card()
+    test = False
+    if (len(sys.argv) == 2):
+        if ('-t' in sys.argv[1]):            
+            test = True
+    else:
+        test = False
+    find_card(test)
--- src/card_reader/tmptst.py
+++ src/card_reader/tmptst.py
@@ -1,21 +1,56 @@
 import cv2 as cv
 import numpy as np
 # from picamera import PiCamera
-def sort_by_dist(contours):
-    for cont in contours:
-        print(cont)
+class Test:
+    def __init__(self, comp):
+        self.comp = comp
+    
+    def sort_by_dist(self,cont):
+        # centers = []
+        # for cont in contours:
+        comp = self.comp
+        M = cv.moments(cont)
+        # print(M['m10'])
+        # print(M['m00'])
+        # print(M['m01'])
+        x = M['m10']/M['m00']
+        y = M['m01']/M['m00']
+        center_this = [x,y]
+        
+        M_comp = cv.moments(comp)
+        # print(M_comp['m10'])
+        # print(M_comp['m00'])
+        # print(M_comp['m01'])
+        x_comp = M_comp['m10']/M_comp['m00']
+        y_comp = M_comp['m01']/M_comp['m00']
+        center_comp = [x_comp,y_comp]
+        
+        dx = center_this[0] - center_comp[0]
+        dy = center_this[1] - center_comp[1]
+        D = np.sqrt(dx*dx+dy*dy)
+        return D
 
+    # def __sort_match(self,cont):
+    #     comp = self.comp
+    #     match = cv.match(cont[-1],comp[-1],cv.CONTOURS_MATCH_I1,420.69)
+
+    # def sort_match(self,contours):
+    #     for i,cont in enumerate(contours):
+    #         if i > 0:
+    #             comp = contours[i-1]
+    #             match = cv.match(cont,comp,cv.CONTOURS_MATCH_I1,420.69)
+            
 def prep(im, mode=True, window='working'):
       # Convert to gray and blur it
     gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(gray,(0,0),2)
     # Adaptive thresholding
     thresh = cv.adaptiveThreshold(blur,255,1,1,11,1)
-    cv.imshow(window,thresh)
-    cv.waitKey(0)
+    # cv.imshow(window,thresh)
+    # cv.waitKey(0)
     
     kernel = np.ones((5,5),np.uint8)
-    # test_kernel = np.ones((3,3),np.uint8)
+    # test_kernel = np.ones((1,1),np.uint8)
     ero = cv.erode(thresh,kernel,iterations=1)
     cv.imshow(window,ero)
     cv.waitKey(0)
@@ -29,22 +64,40 @@ def prep(im, mode=True, window='working'):
     edges = cv.Canny(dil,0,255)
     mask = edges != 0
     dst = im * (mask[:,:,None].astype(im.dtype))
+    cv.imshow(window,dst)
+    cv.waitKey(0)
+
+    # ero2 = cv.erode(dst,test_kernel,iterations=1)
+    # cv.imshow(window,ero2)
+    # cv.waitKey(0)
     _, contours, _ = cv.findContours(edges,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
-    cont_sort = sorted(contours,key=cv.contourArea,reverse=True)[:2]
-    # print('size: ',(cont_sort[i].size for i in cont_sort))
-    print(len(contours))
-    sort_by_dist(contours)
-    for i,cont in enumerate(cont_sort):
+    cont_sort = sorted(contours,key=cv.contourArea,reverse=True)[:5]
+    for cont in cont_sort:
+        print(cv.contourArea(cont))
+    tmp = Test(cont_sort[-1])
+    # print(COMP)
+    cont_dist_sort = sorted(cont_sort,key=tmp.sort_by_dist)
+
+    for i,cont in enumerate(cont_dist_sort):
+        print('distance {}: '.format(i),tmp.sort_by_dist(cont))
         print('size {}: '.format(i),cont.size)
+        print('shape {}: '.format(i),cont.shape)
+    # for i,cont in enumerate(cont_sort):
+    #     print('size {}: '.format(i),cont.size)
 
     draw_all = np.zeros_like(im)
     draw_one = np.zeros_like(im)
-    cv.drawContours(draw_all,contours,-1,(255,255,0),2)
+    draw_test = np.zeros_like(im)
+    cv.drawContours(draw_all,cont_sort[:-2],-1,(128,128,0),3)
+    # cv.drawContours(draw_all,contours[1:],-1,(255,255,0),2)
     cv.drawContours(draw_one,cont_sort,-1,(255,255,0),2)
+    cv.drawContours(draw_test,cont_dist_sort[-2:],-1,(255,255,0),2)
     cv.imshow(window,draw_all)
     cv.waitKey(0)
-
     cv.imshow(window,draw_one)
+    cv.waitKey(0)
+
+    cv.imshow(window,draw_test)
     cv.waitKey(0)
 
 
@@ -53,4 +106,4 @@ _, im = cam.read()
 cv.imshow('test',im)
 cv.waitKey(0)
 prep(im,True,'rank')
-prep(im,False,'suit')
+# prep(im,False,'suit')
